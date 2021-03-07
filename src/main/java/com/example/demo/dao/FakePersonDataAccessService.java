7ab936c7f965d09fe366b458c839ecdfbc5405ea
Luca Gazzola <lgazzola87@gmail.com>
2021-03-07 23:18:00 +0100

    first commit

---
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,58 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao
+{
+  private static List<Person> DB = new ArrayList<>();
+
+  @Override
+  public int insertPerson(UUID id, Person person)
+  {
+    DB.add(new Person(id, person.getName()));
+    return 1;
+  }
+
+  @Override
+  public List<Person> selectAllPerople()
+  {
+    return DB;
+  }
+
+  @Override
+  public int deletePersonById(UUID id)
+  {
+    Optional<Person> person = selectPersonById(id);
+    if (person.isEmpty())
+    {
+      return 0;
+    }
+    DB.remove(person.get());
+    return 1;
+  }
+
+  @Override
+  public int updatePersonById(UUID id, Person person)
+  {
+    Optional<Person> personToUpdate = selectPersonById(id);
+    if (!personToUpdate.isEmpty())
+    {
+      DB.set(DB.indexOf(personToUpdate.get()), new Person(id, person.getName()));
+      return 1;
+    }
+    return 0;
+  }
+
+  @Override
+  public Optional<Person> selectPersonById(UUID id)
+  {
+    return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
+  }
+}
---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,61 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao
+{
+
+  private final JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  public PersonDataAccessService(JdbcTemplate jdbcTemplate)
+  {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @Override
+
+  public int insertPerson(UUID id, Person person)
+  {
+    return 0;
+  }
+
+  @Override
+  public List<Person> selectAllPerople()
+  {
+    RowMapper<Person> tRowMapper = (resultSet, i) ->
+      new Person(UUID.fromString(resultSet.getString("id")), resultSet.getString("name"));
+    return jdbcTemplate.query("SELECT id, name FROM person", tRowMapper);
+  }
+
+  @Override
+  public int deletePersonById(UUID id)
+  {
+    return 0;
+  }
+
+  @Override
+  public int updatePersonById(UUID id, Person person)
+  {
+    return 0;
+  }
+
+  @Override
+  public Optional<Person> selectPersonById(UUID id)
+  {
+    String sql = "SELECT id, name FROM person WHERE ?";
+    RowMapper<Person> tRowMapper = (resultSet, i) ->
+      new Person(UUID.fromString(resultSet.getString("id")), resultSet.getString("name"));
+    Person person = jdbcTemplate.queryForObject(sql, tRowMapper, id);
+    return Optional.ofNullable(person);
+  }
+}
