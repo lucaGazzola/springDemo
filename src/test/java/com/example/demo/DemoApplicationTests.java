440bceabce221f03fdacdf38ad1e8609e1ad1154
Luca Gazzola <lgazzola87@gmail.com>
2021-03-20 17:29:31 +0100

    using docker-compose-rule to bring up psql container before tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,21 @@
 package com.example.demo;
 
+import com.palantir.docker.compose.DockerComposeExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests
+public class DemoApplicationTests
 {
 
+  @RegisterExtension
+  public static DockerComposeExtension docker = DockerComposeExtension.builder()
+    .file("docker-compose.yml")
+    .build();
+
+  @Test
+  public void contextLoads()
+  {
+  }
 }
