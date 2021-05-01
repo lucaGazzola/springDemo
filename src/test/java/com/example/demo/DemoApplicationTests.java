f0f74e35788baba4f06ceac223679c313f053478
Luca Gazzola <lgazzola87@gmail.com>
2021-05-01 18:50:50 +0200

    added assert on initialized controller

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.api.PersonController;
 import com.palantir.docker.compose.DockerComposeExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class DemoApplicationTests
@@ -14,8 +18,12 @@ public class DemoApplicationTests
     .file("docker-compose.yml")
     .build();
 
+  @Autowired
+  PersonController personController;
+
   @Test
   public void contextLoads()
   {
+    assertThat(personController).isNotNull();
   }
 }
