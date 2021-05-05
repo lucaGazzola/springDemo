5eeee3bf94a8ce05fd854da774ea288ac1a84163
Luca Gazzola <lgazzola87@gmail.com>
2021-05-05 15:42:36 +0200

    now testing with http request

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests
 {
+
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
 
   @RegisterExtension
   public static DockerComposeExtension docker = DockerComposeExtension.builder()
@@ -22,8 +31,10 @@ public class DemoApplicationTests
   PersonController personController;
 
   @Test
-  public void contextLoads()
+  public void simpleRequest()
   {
     assertThat(personController).isNotNull();
+    assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/v1/person", String.class))
+      .contains("[]");
   }
 }
